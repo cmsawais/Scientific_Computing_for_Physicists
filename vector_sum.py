08bdafee56e4a4f8bf8ce974f70a0cabd50ec988
Awais <156588675+cmsawais@users.noreply.github.com>
2025-08-21 12:18:39 +0200

    Delete vector_sum.py

--- vector_sum.py
+++
@@ -1,9 +0,0 @@
-#Vector Sum
-a=3 # scalar
-x= [1]*20 #vector of N=20 dimension
-y= [4]* 20 #vector of N=20 dimension
-z= [a*xi+yi for xi, yi in zip (x,y)]  #Calculates scalar product z=a*x+y 
-with open ("task1.text", "w") as f:
-f.write("z="+str(z)+"\n") #print values on text file
-
-
